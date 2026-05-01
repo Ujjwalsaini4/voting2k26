c5370c0750ac6ed0cdc74802b7736d36deafce17
Ujjwalsaini4 <ujjwalyt71@gmail.com>
2026-05-02 00:11:57 +0530

    fix: change router to BrowserRouter and update base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, lazy, Suspense } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { loginAnonymously } from './utils/firebase';
 
 import Navigation from './components/layout/Navigation';
